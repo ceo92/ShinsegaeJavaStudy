f325beb6cbf56397e11c11f329c2d482ea378aa9
ceo92 <king9243@naver.com>
2024-07-02 01:28:14 +0900

    다형성과제

---
+++ study_shinsegae/src/_0701_inheritance_assignment/shape01/Shape.java
@@ -0,0 +1,83 @@
+package _0701_inheritance_assignment.shape01;
+
+public abstract class Shape {
+
+  protected double area;
+  private String name;
+
+  public Shape(){}
+
+  public Shape(String name){
+    this.name=name;
+  }
+
+  abstract void calculationArea();
+
+  public double getArea() {
+    return area;
+  }
+
+  public void setArea(double area) {
+    this.area = area;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  void print(){
+    System.out.println(name+"의 면적은 "+area);
+  }
+}
+
+class Circle extends Shape{
+  private double radius;
+
+  public Circle(String name , double radius) {
+    super(name);
+    this.radius = radius;
+  }
+
+  public Circle() {
+  }
+
+
+
+  void calculationArea() {
+    area = radius * radius * Math.PI;
+    print();
+  }
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public void setRadius(double radius) {
+    this.radius = radius;
+  }
+}
+
+class Rectangular extends Shape{
+  private double width;
+  private int height;
+
+  public Rectangular() {
+  }
+
+  @Override
+  void calculationArea() {
+    area =  width * height;
+    print();
+  }
+
+  public Rectangular(String name , double width, int height) {
+    super(name);
+    this.width = width;
+    this.height = height;
+
+  }
+}
